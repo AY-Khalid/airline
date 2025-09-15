cef2ddf83449f08f9c0ff6f7c21c29f02da77f8c
unknown <aniduyakubu@yahoo.com>
2025-09-15 16:53:00 +0100

    another fix to the condition

--- flight/models.py
+++ flight/models.py
@@ -18,7 +18,7 @@ class Flight(models.Model):
         return f"Flight {self.id}: from {self.origin} to {self.destination}"
     
     def is_valid_flight(self):
-        return self.origin != self.destination and self.duration > 0
+        return self.origin != self.destination or self.duration > 0
     
 
 class Passenger(models.Model):
